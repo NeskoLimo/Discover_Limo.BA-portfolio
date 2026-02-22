9ce9d5a685c70ef930d91fd9fd1e08978ffad4d3
Bernard Limo <127617357+NeskoLimo@users.noreply.github.com>
2026-02-22 16:36:16 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/global.css';  // ← global tokens & resets first
+import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
